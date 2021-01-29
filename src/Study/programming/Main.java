526cadf09d4e4480f2771b3fc62f39e02c3a7ce9
thangaramanujan <77881820+thangaramanujan@users.noreply.github.com>
2021-01-29 20:35:59 +0530

    -Encapsulation using two classes (Player,EnhancedPlayer) -

---
+++ src/Study/programming/Main.java
@@ -0,0 +1,25 @@
+package Study.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        Player player = new Player();
+//        player.name = "sri";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("sri",200,"Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+}
